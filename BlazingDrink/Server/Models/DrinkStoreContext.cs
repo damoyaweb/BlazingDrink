cd229c76d7dad7eb71b9225f557b5fb5aed35dae
dimoyahi <85351327+damoyaweb@users.noreply.github.com>
2021-07-27 08:39:23 -0500

    Initial commit

---
+++ BlazingDrink/Server/Models/DrinkStoreContext.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore;
+using BlazingDrink.Shared;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BlazingDrink.Server.Models
+{
+	public class DrinkStoreContext:DbContext
+	{
+		public DbSet<DrinkSpecial> Special { get; set; }
+		public DrinkStoreContext(DbContextOptions options) : base(options) { }
+
+	}
+}
---
+++ BlazingDrink/Shared/DrinkSpecial.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BlazingDrink.Shared
+{
+	public class DrinkSpecial
+	{
+		public int Id { get; set; }
+		public string Nombre { get; set; }
+		public decimal PrecioBase { get; set; }
+		public string Descripcion { get; set; }
+		public string ImageUrl { get; set; }
+		public string GetFormattedPrecioBase() => PrecioBase.ToString("0.00");
+	}
+}
