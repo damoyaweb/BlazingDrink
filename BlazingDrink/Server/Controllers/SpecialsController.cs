7e129725829b7a260013201b15af98e55612167a
dimoyahi <85351327+damoyaweb@users.noreply.github.com>
2021-07-27 16:14:58 -0500

    .

---
+++ BlazingDrink/Server/Controllers/SpecialsController.cs
@@ -0,0 +1,27 @@
+﻿using BlazingDrink.Server.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BlazingDrink.Server.Controllers
+{
+	[Route("special")]
+	[ApiController]
+	public class SpecialsController : ControllerBase
+	{
+		private readonly DrinkStoreContext Context;
+		public SpecialsController(DrinkStoreContext context) =>
+			Context = context;
+		
+		[HttpGet]
+		public async Task<ActionResult<DrinkSpecial>>> GetSpecial()
+		{
+
+		}
+
+
+	}
+}
