f862b43e291bbeba2af32c9a5e8abf66dfce85ac
Diego Moya <dimoyahi@outlook.es>
2021-07-27 22:51:12 -0500

    .

--- BlazingDrink/Client/obj/Debug/net5.0/RazorDeclaration/Pages/Index.razor.g.cs
+++ BlazingDrink/Client/obj/Debug/net5.0/RazorDeclaration/Pages/Index.razor.g.cs
@@ -13,77 +13,77 @@ namespace BlazingDrink.Client.Pages
     using System.Threading.Tasks;
     using Microsoft.AspNetCore.Components;
 #nullable restore
-#line 1 "C:\DISCO_Datos\BlazingDrink\BlazingDrink\BlazingDrink\Client\_Imports.razor"
+#line 1 "C:\Diego Moya\Proyectos\BlazingDrink\BlazingDrink\Client\_Imports.razor"
 using System.Net.Http;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 2 "C:\DISCO_Datos\BlazingDrink\BlazingDrink\BlazingDrink\Client\_Imports.razor"
+#line 2 "C:\Diego Moya\Proyectos\BlazingDrink\BlazingDrink\Client\_Imports.razor"
 using System.Net.Http.Json;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 3 "C:\DISCO_Datos\BlazingDrink\BlazingDrink\BlazingDrink\Client\_Imports.razor"
+#line 3 "C:\Diego Moya\Proyectos\BlazingDrink\BlazingDrink\Client\_Imports.razor"
 using Microsoft.AspNetCore.Components.Forms;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 4 "C:\DISCO_Datos\BlazingDrink\BlazingDrink\BlazingDrink\Client\_Imports.razor"
+#line 4 "C:\Diego Moya\Proyectos\BlazingDrink\BlazingDrink\Client\_Imports.razor"
 using Microsoft.AspNetCore.Components.Routing;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 5 "C:\DISCO_Datos\BlazingDrink\BlazingDrink\BlazingDrink\Client\_Imports.razor"
+#line 5 "C:\Diego Moya\Proyectos\BlazingDrink\BlazingDrink\Client\_Imports.razor"
 using Microsoft.AspNetCore.Components.Web;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 6 "C:\DISCO_Datos\BlazingDrink\BlazingDrink\BlazingDrink\Client\_Imports.razor"
+#line 6 "C:\Diego Moya\Proyectos\BlazingDrink\BlazingDrink\Client\_Imports.razor"
 using Microsoft.AspNetCore.Components.Web.Virtualization;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 7 "C:\DISCO_Datos\BlazingDrink\BlazingDrink\BlazingDrink\Client\_Imports.razor"
+#line 7 "C:\Diego Moya\Proyectos\BlazingDrink\BlazingDrink\Client\_Imports.razor"
 using Microsoft.AspNetCore.Components.WebAssembly.Http;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 8 "C:\DISCO_Datos\BlazingDrink\BlazingDrink\BlazingDrink\Client\_Imports.razor"
+#line 8 "C:\Diego Moya\Proyectos\BlazingDrink\BlazingDrink\Client\_Imports.razor"
 using Microsoft.JSInterop;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 9 "C:\DISCO_Datos\BlazingDrink\BlazingDrink\BlazingDrink\Client\_Imports.razor"
+#line 9 "C:\Diego Moya\Proyectos\BlazingDrink\BlazingDrink\Client\_Imports.razor"
 using BlazingDrink.Client;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 10 "C:\DISCO_Datos\BlazingDrink\BlazingDrink\BlazingDrink\Client\_Imports.razor"
+#line 10 "C:\Diego Moya\Proyectos\BlazingDrink\BlazingDrink\Client\_Imports.razor"
 using BlazingDrink.Client.Shared;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 11 "C:\DISCO_Datos\BlazingDrink\BlazingDrink\BlazingDrink\Client\_Imports.razor"
+#line 11 "C:\Diego Moya\Proyectos\BlazingDrink\BlazingDrink\Client\_Imports.razor"
 using BlazingDrink.Shared;
 
 #line default
@@ -98,7 +98,7 @@ using BlazingDrink.Shared;
         }
         #pragma warning restore 1998
 #nullable restore
-#line 7 "C:\DISCO_Datos\BlazingDrink\BlazingDrink\BlazingDrink\Client\Pages\Index.razor"
+#line 7 "C:\Diego Moya\Proyectos\BlazingDrink\BlazingDrink\Client\Pages\Index.razor"
       
     List<DrinkSpecial> Specials;
 
