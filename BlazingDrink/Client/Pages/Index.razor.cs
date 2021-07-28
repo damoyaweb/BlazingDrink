2f01fe80459f2e64fce503c9013bc5f77c4dd3a7
dimoyahi <85351327+damoyaweb@users.noreply.github.com>
2021-07-28 14:12:42 -0500

    blazor

---
+++ BlazingDrink/Client/Pages/Index.razor.cs
@@ -0,0 +1,25 @@
+﻿using BlazingDrink.Shared;
+using Microsoft.AspNetCore.Components;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Net.Http.Json;
+using System.Threading.Tasks;
+
+namespace BlazingDrink.Client.Pages
+{
+	public partial class Index
+	{
+        [Inject]
+        public HttpClient HttpClient { get; set; }
+
+        List<DrinkSpecial> Specials;
+
+        protected async override Task OnInitializedAsync()
+        {
+            Specials = await HttpClient
+            .GetFromJsonAsync<List<DrinkSpecial>>("special");
+        }
+    }
+}
