e166aa7f553ad514110d8b9036242f85e45fecad
dimoyahi <85351327+damoyaweb@users.noreply.github.com>
2021-08-04 16:34:09 -0500

    .

--- BlazingDrink/Client/Shared/ConfigureDrinkDialog.razor.cs
+++ BlazingDrink/Client/Shared/ConfigureDrinkDialog.razor.cs
@@ -3,15 +3,30 @@ using Microsoft.AspNetCore.Components;
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net.Http;
+using System.Net.Http.Json;
 using System.Threading.Tasks;
 
 namespace BlazingDrink.Client.Shared
 {
 	public partial class ConfigureDrinkDialog
 	{
+		#region Servivios
+		[Inject]
+		public HttpClient HttpClient { get; set; }
+		#endregion
 		#region Parametros
 		[Parameter]
-		public Drink Drink { get; set; } 
+		public Drink Drink { get; set; }
+		#endregion
+		#region Variables
+		IEnumerable<Topping> Toppings;
+		#endregion
+		#region Overrides
+		protected async override Task OnInitializedAsync()
+		{
+			Toppings = await HttpClient.GetFromJsonAsync<IEnumerable<Topping>>("toppings");
+		}
 		#endregion
 	}
 }
---
+++ BlazingDrink/Server/Controllers/ToppingsController.cs
@@ -0,0 +1,29 @@
+﻿using BlazingDrink.Server.Models;
+using BlazingDrink.Shared;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BlazingDrink.Server.Controllers
+{
+	[Route("toppings")]
+	[ApiController]
+	public class ToppingsController : ControllerBase
+	{
+		private readonly DrinkStoreContext Context;
+		public ToppingsController(DrinkStoreContext context)
+		{
+			Context = context;
+		}
+		[HttpGet]
+		public async Task<ActionResult<List<Topping>>> GetToppings()
+		{
+			return await Context.Toppings
+				.OrderBy(t => t.Nombre).ToListAsync();
+		}
+	}
+}
